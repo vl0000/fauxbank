c902f3e4f424de80e9761cec3014dd349aaee2bd
vl0000 <lvictor255@protonmail.com>
2023-12-27 11:05:39 -0300

    - Updated Packages - Installed Capactior - Now the AccountInfo component fetches the name from a temprary backend[broken]

---
+++ src/frontend/src/stores.js
@@ -0,0 +1,33 @@
+import { readable } from "svelte/store";
+
+// Everything in this file is temporary
+const default_values = {
+    full_name: "NOTHING",
+    balance: 0.0,
+    agency: 1,
+    account_number: 0
+}
+async function get_account_data() {
+    let resp = await fetch(
+        "http://localhost:8000/api/account/1?query=full_name",
+        {
+            headers: {
+                method: "GET",
+                cors: "no-cors"
+            }
+        }
+    ).then(resp => resp.json())
+    console.log(resp.full_name)
+    return resp.full_name
+}
+// refactor this so that only balance and transaction stores are polled repeatedly
+export let account = readable(default_values, function start(set) {
+    const interval = setInterval(async() => {
+        let newval = await get_account_data()
+        set(newval)
+    }, 5000)
+
+    return function stop() {
+        clearInterval(interval)
+    }
+})
