6a2fb59a1615627ba4ddac3f2e0059ac127c59c0
vl0000 <lvictor255@protonmail.com>
2024-01-02 13:56:11 -0300

    - Changed the name of the Account model to AccountInDB to prep for the usage of multiple models - Modified AccountInDB to hash passwords before attempting to insert them into the database - Added the password and salt fields for security - Installed two new modules. python-jose for jwt tokens and passlib to simplify password hashing

--- src/backend/database/models.py
+++ src/backend/database/models.py
@@ -1,21 +1,34 @@
 from pydantic import BaseModel
 from database.config import DB
 from random import randint
+from secrets import token_bytes
+from passlib.hash import bcrypt
+from base64 import b64encode
 
-class Account(BaseModel):
-    full_name: str | None = None
+def hash_password(password: str, salt: str):
+    if not password:
+        raise ValueError("Password cannot be null")
+    else:
+        password = bcrypt.using(rounds=8).hash(password + salt)
+
+
+class AccountInDb(BaseModel):
+    full_name: str
     balance: float = 0.0
-    agency: int | None = 1
+    agency: int = 1
     account_number: int = randint(1, 9999999999)
-    email: str | None = None
+    email: str
+    password: str
+    # If the salt isnt turned into a string, it cannot be appended to the password later
+    salt: str = b64encode(token_bytes(16)).decode("utf-8")
 
     #TODO Turn create into a class model for a base DB class inheriting from BaseModel.
     def create(self):
-        tp = tuple(self.model_dump().values())
-        DB.query(f"INSERT INTO account VALUES (NULL, ?, ?, ?, ?, ?)", tp)
+        temp_model = self.model_dump()
+        temp_model['password'] = hash_password(temp_model['password'], temp_model['salt'])
 
+        DB.query(f"INSERT INTO account VALUES (NULL, ?, ?, ?, ?, ?)", tuple(temp_model.values()))
 
-            
     @classmethod
     def get_user(cls, email: str):
         return DB.query(f"SELECT * FROM account WHERE email = ?", (email,))
--- src/backend/routes/account.py
+++ src/backend/routes/account.py
@@ -1,20 +1,23 @@
 from typing import Annotated
 
-from database.models import Account
+from database.models import AccountInDb
 
-from fastapi import APIRouter, Query, Form
+from fastapi import APIRouter, Query, Form, Depends
 from fastapi.responses import JSONResponse
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 # TEMPORARY!!
 # TODO REMOVE THIS. After a db is made
-acc = Account(
+acc = AccountInDb(
     full_name= "Fulano Fulanildo",
     balance= 345.97,
     agency= 1,
     account_number= 1234
 )
 
-router = APIRouter()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+router = APIRouter(dependencies=[Depends(oauth2_scheme)])
 
 @router.get("/api/account/{id}")
 def get_account(id: int | None, query: Annotated[list[str], Query()] = None):
@@ -28,17 +31,17 @@ def get_account(id: int | None, query: Annotated[list[str], Query()] = None):
     return JSONResponse(content=response)
 
 @router.post("/api/account/token")
-def login(email: Annotated[str, Form()], password: Annotated[str, Form()]):
+def login(formData: Annotated[OAuth2PasswordRequestForm, Depends()]):
 
-    print(email,password)
+    print(formData.username,formData.password)
     #TODO Actual login
-    if email and password and Account.get_user(email):
+    if formData and AccountInDb.get_user(email=formData.username):
         return { "success" : True }
 
 @router.post("/api/signup")
 def signup(full_name: Annotated[str, Form()],email: Annotated[str, Form()], password: Annotated[str, Form()]):
 
     #TODO Implement validation
-    user = Account(full_name=full_name, email=email, password=password)
+    user = AccountInDb(full_name=full_name, email=email, password=password)
     user.create()
     return {"success" : True }
