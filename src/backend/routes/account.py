5afee76038bb0d91303b4d8e219e4f81d9f8caf7
vl0000 <lvictor255@protonmail.com>
2024-01-04 11:17:49 -0300

    Added an api prefix

--- src/backend/routes/account.py
+++ src/backend/routes/account.py
@@ -3,18 +3,20 @@ from typing import Annotated
 from database.models import AccountInDb, Token
 
 from fastapi import APIRouter, Query, Form, Depends
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, Response
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-router = APIRouter(dependencies=[])
+router = APIRouter(prefix="/api/account")
 
 acc = {
     "full_name": "fulano"
 }
 
-@router.get("/api/account/{id}")
+# This is a security issure right now, but this route wont even exist in the future anyways.
+# It is for testing purposes only
+@router.get("/{id}")
 def get_account(id: int | None, token: Annotated[oauth2_scheme, Depends()], query: Annotated[list[str], Query()] = None):
     response = dict()
     if query:
@@ -25,18 +27,17 @@ def get_account(id: int | None, token: Annotated[oauth2_scheme, Depends()], quer
     
     return JSONResponse(content=response)
 
-@router.post("/api/account/token", response_model=Token)
-def login(formData: Annotated[OAuth2PasswordRequestForm, Depends()]):
+@router.post("/token", response_model=Token)
+def login(response: Response,formData: Annotated[OAuth2PasswordRequestForm, Depends()]):
     print("here")
     user = AccountInDb.authenticate(formData.username, formData.password)
     print(user)
     if user:
         # The email is the 5th element in the returned tuple
         access_token = Token.generate_token(data={"sub": user[5]})
-    
     return {"access_token": access_token, "token_type": "bearer"}
 
-@router.post("/api/signup")
+@router.post("/signup")
 def signup(full_name: Annotated[str, Form()],email: Annotated[str, Form()], password: Annotated[str, Form()]):
 
     #TODO Implement validation
