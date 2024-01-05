e70e761afe9a8ff6801868048d9dc35e601756d9
vl0000 <lvictor255@protonmail.com>
2024-01-05 19:35:20 -0300

    Implemented a connection pool for the database

--- src/backend/database/config.py
+++ src/backend/database/config.py
@@ -1,13 +1,36 @@
 import sqlite3
+import threading
+
+from queue import Queue
 
 from os import environ
 
-class Database:
-    def __init__(self, DB_name) -> None:
+class ConnectionPool:
+    def __init__(self, DB_name: str) -> None:
         self.DB_name = DB_name
+        self.pool = Queue(maxsize=2)
+
+        for _ in range(2):
+            self.pool.put(
+                sqlite3.connect(DB_name)
+            )
+    
+    def get_connection(self) -> None:
+        if not self.pool.empty():
+            return self.pool.get(timeout=5.0)
+        else:
+            raise RuntimeError("The connection pool is empty")
+    
+    def disconnect(self, connection: sqlite3.Connection) -> None:
+        self.pool.put(connection)
+
+
+class Database:
+    def __init__(self, pool: ConnectionPool) -> None:
+        self.pool = pool
     
     def query(self, query: str, data: tuple = None) -> list[dict] | None:
-        CONN = sqlite3.connect(self.DB_name)
+        CONN = self.pool.get_connection()
         CURSOR = CONN.cursor()
 
         try:
@@ -22,13 +45,14 @@ class Database:
         finally:
             result = CURSOR.fetchall()
             CONN.commit()
-            CURSOR.close()
-            CONN.close()
+            self.pool.disconnect(CONN)
             if result:
                 return result
     
 
-DB = Database("db.sqlite")
+connection_pool = ConnectionPool("db.sqlite")
+
+DB = Database(connection_pool)
 
 
 
