f73cad64bbf673bf794423ac779a55c9e84b840b
vl0000 <lvictor255@protonmail.com>
2024-01-18 10:41:26 -0300

    removed the testing view

--- src/backend/main.py
+++ src/backend/main.py
@@ -14,9 +14,5 @@ app.add_middleware(
     allow_headers=["Authorization"]
 )
 
-@app.get("/")
-def test_route():
-    return {"Hello": "World!"}
-
 if __name__ == "__main__":
     run("main:app", host="0.0.0.0", port=5000, log_level="info")
