34c2572058e57f5e2dee286d9fa79bd2d25020d2
vl0000 <lvictor255@protonmail.com>
2024-01-18 12:48:05 -0300

    flagged ssl as required for the database connection

--- src/backend/database/tables.py
+++ src/backend/database/tables.py
@@ -1,7 +1,7 @@
 from os import environ
 from sqlalchemy import MetaData, Table, Column, BigInteger, String, Float, ForeignKey, DateTime, Index, create_engine
 
-engine = create_engine(environ["POSTGRES_URL"])
+engine = create_engine(environ["POSTGRES_URL"], connect_args={"sslmode": "require"})
 
 
 meta = MetaData()
